8adae05d11a7ab3063a0e1c4fcdff8a48d084423
Felipe <dfsilvat@gmail.com>
2024-04-19 15:47:20 -0300

    fix: bug menu

--- app/components/Hamburguer/index.tsx
+++ app/components/Hamburguer/index.tsx
@@ -13,7 +13,7 @@ const Hamburguer = () => {
                     <a href="https://my-web-site-chi.vercel.app/" target='_blank'>Conheça o desenvolvedor!</a>
                 </li>
                 <li>
-                    <a href="">Entre em contato!</a>
+                    <a href="https://api.whatsapp.com/send/?phone=5581991274483&text=Ol%C3%A1%2C+preciso+de+um+or%C3%A7amento%21&type=phone_number&app_absent=0">Entre em contato!</a>
                 </li>
             </ul>
         </div>
